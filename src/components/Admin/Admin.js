abce6c024ee1e88ed55304dbfd1c078f26d5680e
jattoabdul <jattoade@gmail.com>
2018-11-18 23:29:28 +0100

    make table scrollable on tablet and mobile

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -275,60 +275,60 @@ class Admin extends Component {
                     </div>
                     {/** main section */}
                     <div className="blog-list-table-section">
-                    <div className="table-filters">
-                        <InputGroup className="search-filter">
-                            <Label for="search">Search</Label>
-                            <Input className="search-input" placeholder="Search" name="search" id="search" />
-                        </InputGroup>
-                        <InputGroup className="no-of-record-filter">
-                            <Label for="no-of-record">No of Post</Label>
-                            <Input className="select-elem" type="select" name="select" id="no-of-record" defaultValue={'10'} value={this.state.postPerPage} onChange={this.handleOnChangeNoPerPage}>
-                            <option value="1">1</option>
-                            <option value="10">10</option>
-                            <option value="25">25</option>
-                            <option value="50">50</option>
-                            <option value="100">100</option>
-                            <option value={totalCount || "1000"}>All</option>
-                            </Input>
-                        </InputGroup>
-                        <InputGroup className="category-filter">
-                            <Label for="categories">Category</Label>
-                            <Input className="select-elem" type="select" name="select" id="categories" value={currentCategory} onChange={this.handleOnSelectCategory}>
-                            {categories && categories.map((category, key) =>
-                                <option value={`${category.id}`} key={key}>{category.name}</option>)}
-                            <option value="All">All</option>
-                            </Input>
-                        </InputGroup>
+                        <div className="table-filters">
+                            <InputGroup className="search-filter">
+                                <Label for="search">Search</Label>
+                                <Input className="search-input" placeholder="Search" name="search" id="search" />
+                            </InputGroup>
+                            <InputGroup className="no-of-record-filter">
+                                <Label for="no-of-record">No of Post</Label>
+                                <Input className="select-elem" type="select" name="select" id="no-of-record" defaultValue={'10'} value={this.state.postPerPage} onChange={this.handleOnChangeNoPerPage}>
+                                <option value="1">1</option>
+                                <option value="10">10</option>
+                                <option value="25">25</option>
+                                <option value="50">50</option>
+                                <option value="100">100</option>
+                                <option value={totalCount || "1000"}>All</option>
+                                </Input>
+                            </InputGroup>
+                            <InputGroup className="category-filter">
+                                <Label for="categories">Category</Label>
+                                <Input className="select-elem" type="select" name="select" id="categories" value={currentCategory} onChange={this.handleOnSelectCategory}>
+                                {categories && categories.map((category, key) =>
+                                    <option value={`${category.id}`} key={key}>{category.name}</option>)}
+                                <option value="All">All</option>
+                                </Input>
+                            </InputGroup>
+                        </div>
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Post ID</th>
+                                    <th scope="col">Post Title</th>
+                                    <th scope="col">Short Description</th>
+                                    <th scope="col">Thumbnail Url</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Published At</th>
+                                    <th scope="col">Created At</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {isFetchingBlogPosts ? <tr><td align="center" colSpan="10" className="loader">Loading....</td></tr> : 
+                                (parseInt(totalCount, 10) === 0 ? <tr><td align="center" colSpan="10">No Posts at the moment</td></tr> : renderPosts)}
+                            </tbody>
+                        </table>
+                        {(
+                            parseInt(totalCount, 10) === 0 ?
+                            '' :
+                            <Pagination
+                                handlePageClick={this.handlePageClick}
+                                pageCount={parseInt(pageCount, 10)}
+                            />
+                        )}
                     </div>
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">Post ID</th>
-                                <th scope="col">Post Title</th>
-                                <th scope="col">Short Description</th>
-                                <th scope="col">Thumbnail Url</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Published At</th>
-                                <th scope="col">Created At</th>
-                                <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {isFetchingBlogPosts ? <tr><td align="center" colSpan="10" className="loader">Loading....</td></tr> : 
-                            (parseInt(totalCount, 10) === 0 ? <tr><td align="center" colSpan="10">No Posts at the moment</td></tr> : renderPosts)}
-                        </tbody>
-                    </table>
-                    {(
-                        parseInt(totalCount, 10) === 0 ?
-                        '' :
-                        <Pagination
-                            handlePageClick={this.handlePageClick}
-                            pageCount={parseInt(pageCount, 10)}
-                        />
-                    )}
-                </div>
 
-                <ModalContainer isModalOpen={this.state.isModalOpen} toggle={this.toggle} actionName={this.state.actionName} addPostAction={this.addPostAction} deletePost={this.deletePostAction} updatePost={this.updatePostAction} post={this.state.singlePost} onChange={this.handleOnChange} onCheckBox={this.handleOnCheckBox} onMultiSelect={this.handleOnMultiSelect} onHandleEditorChange={this.handleEditorChange} onUpload={this.handleUpload} postCategories={categories} />
+                    <ModalContainer isModalOpen={this.state.isModalOpen} toggle={this.toggle} actionName={this.state.actionName} addPostAction={this.addPostAction} deletePost={this.deletePostAction} updatePost={this.updatePostAction} post={this.state.singlePost} onChange={this.handleOnChange} onCheckBox={this.handleOnCheckBox} onMultiSelect={this.handleOnMultiSelect} onHandleEditorChange={this.handleEditorChange} onUpload={this.handleUpload} postCategories={categories} />
                 </section>
             </div>,
             <Footer key={'footer'} email={email} handleSubscribeInput={this.handleSubscribeInput} handleSubscribeSubmit={this.handleSubscribeSubmit} />
